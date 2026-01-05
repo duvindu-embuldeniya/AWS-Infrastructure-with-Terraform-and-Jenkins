a98422bd58ef652a4a7a66ddfbc44fd5078930fb
duvindu-embuldeniya <duvindu.personal@gmail.com>
2026-01-05 09:12:54 +0530

    added new vm

--- simply/settings.py
+++ simply/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '56.228.10.226', 'app.duvindu.org', 'jenkins.duvindu.org', 'duvindu.org', 'www.duvindu.org']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '13.51.159.164', 'app.duvindu.org', 'jenkins.duvindu.org', 'duvindu.org', 'www.duvindu.org']
 
 
 # Application definition
